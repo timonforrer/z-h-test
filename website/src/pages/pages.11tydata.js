6d1c65b0eb0a1dd5846c272de3a1a8ca6b3ecb82
timonforrer <timon.forrer@gmail.com>
2022-02-13 21:27:49 +0100

    Shared data for pages

--- website/src/pages/pages.11tydata.js
+++ website/src/pages/pages.11tydata.js
@@ -1,3 +1,16 @@
+
+// Diese Datei gilt für alle .njk-Files im Ordner 'pages'
+
 module.exports = {
-  layout: "base.njk"
+  layout: "base.njk",
+  eleventyComputed: {
+    title: data => {
+      return data._data.pageBase.title
+    },
+    permalink: data => {
+      [data._data.pageBase.slug];
+      const slug = data._data.pageBase.slug?.current ?? null;
+      return slug !== null ? `${slug}/index.html` : 'index.html';
+    }
+  }
 }
